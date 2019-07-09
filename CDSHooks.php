c7bd86d6586d67048fb9951358f8e3deb30b3ec5
Mark McEver <mark@fastmail.net>
2019-07-09 09:08:46 -0500

    Implemented a services list call and a patient-view service

--- CDSHooks.php
+++ CDSHooks.php
@@ -2,5 +2,52 @@
 namespace Vanderbilt\CDSHooks;
 
 class CDSHooks extends \ExternalModules\AbstractExternalModule{
+	function listServices(){
+		$this->sendJSONResponse([
+			"services" => [
+				[
+					'hook' => 'patient-view',
+					'name' => 'REDCap CDS Service Example',
+					'description' => 'A simple REDCap CDS Service example.',
+					'id' => 'patient-view',
+					"prefetch" => [
+						'patientToGreet' => 'Patient/{{Patient.id}}'
+					]
+				]
+			]
+		]);
+	}
 
+	function patientView(){
+		$this->sendJSONResponse([
+			"cards" => [
+				[
+					'summary' => 'REDCap Example Card',
+					'indicator' => 'success',
+					'detail' => 'This is an example CDS Hooks response from REDCap.',
+					'source' => [
+						'label' => 'REDCap',
+						'url' => APP_PATH_WEBROOT_FULL
+					],
+					'links' => [
+						[
+							'label' => 'REDCap Example Link',
+							'url' => APP_PATH_WEBROOT_FULL,
+							'type' => 'absolute'
+						]
+					]
+				]
+			]
+		]);
+	}
+
+	function sendJSONResponse($content){
+		header('Content-Type: application/json');
+		die(json_encode($content, JSON_PRETTY_PRINT));
+	}
+
+	function sendErrorResponse($content){
+		http_response_code(400);
+		die($content);
+	}
 }
---
+++ services.php
@@ -0,0 +1,19 @@
+<?php
+
+$cdsUrl = $_GET['cds-url'];
+$parts = explode('/', $_GET['cds-url']);
+$serviceId = @$parts[2];
+
+if($parts[1] !== 'cds-services'){
+	$module->sendErrorResponse("A URL ending in 'cds-url=/cds-services' was expected.");
+}
+
+if(empty($serviceId)){
+	$module->listServices();
+}
+else if($serviceId === 'patient-view'){
+	$module->patientView();
+}
+else{
+	$module->sendErrorResponse("A service could not be found with the following id: $serviceId");
+}
